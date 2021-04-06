b1b6433f61082734de1e76a311b4b3c9e6a844df
yj-oh <holidayoh99@gmail.com>
2021-04-07 02:02:18 +0900

    [추가] firebase.js
    #1

---
+++ firebase.js
@@ -0,0 +1,38 @@
+const firebaseConfig = {
+	apiKey: '',
+	projectId: '',
+	storageBucket: '',
+	appId: '',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+const collectionName = 'score';
+
+export const getRanking = async () => {
+	let data = [];
+
+	await db.collection(collectionName)
+		.orderBy('score', 'desc')
+		.limit(5)
+		.get()
+		.then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				data.push(doc.data());
+			});
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+	return data;
+}
+
+export const setRanking = async (username, score) => {
+	await db.collection(collectionName)
+		.add({
+			username: username,
+			score: score,
+		});
+}
